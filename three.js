632e3234f3568c9adc262034fa6a3fadf7ab7a04
Ankit chaurasiya <140043209+7632811966@users.noreply.github.com>
2023-08-25 20:09:09 +0530

    Add files via upload
    operator

---
+++ three.js
@@ -0,0 +1,12 @@
+let a = 100
+let b=  '100'
+
+let c = "Ram"
+let d ="syam"
+console.log(c==d)  //false
+console.log(c===d) //false
+//string == string  && rahul == Rahul
+//  true && false 
+//   false
+console.log(a==b)
+console.log(a===b)
