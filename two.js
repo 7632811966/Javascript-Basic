9b9a786004c719555f23b9677994c6c6b999edeb
Ankit chaurasiya <140043209+7632811966@users.noreply.github.com>
2023-08-25 20:11:52 +0530

    Add files via upload
    datatype

--- two.js
+++ two.js
@@ -1,18 +1,26 @@
-let emp={
-    id:101,
-    name:"ankit",
-    sal:45000
+let size =[ 'M', 'L', 'XL']
+
+
+let ids = [101,102,103]
+let sizes = [38,40,42,44]
+
+let emp = {}  
+let product = {
+    id:'p101',
+    p_Name:"Iphone 18",
+    price :18000
 }
 
-console.log(emp)
-//object value
-//object.property
-console.log(emp.id)
-console.log(emp.name)
-console.log(emp.sal)
-console.log(emp.loc)
 
-emp.loc = "japan"
-emp.name = "syam sundar"
-
+console.log(size)
+console.log(sizes)
+console.log(ids)
 console.log(emp)
+console.log(product)
+console.log("*****")
+
+console.log(typeof size)
+console.log(typeof sizes)
+console.log(typeof ids)
+console.log(typeof emp)
+console.log(typeof product)
